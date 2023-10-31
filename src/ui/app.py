a0437902c2beecea89ed408d2e7b7d4f9510a8ea
Luis Canada <canadaluis5@gmail.com>
2023-10-31 09:33:21 -0400

    Yippe games!

--- src/ui/app.py
+++ src/ui/app.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 
 from src.gameList import gameList
+from src.ui.game import Game
 
 # from src.ui.game import Game
 
@@ -17,20 +18,11 @@ MDScreen:
             cols: 5
             id: images_grid
             size_hint_y: None
-            height: self.minimum_height
-            width: self.minimum_width
+            height: self.minimum_height  #<<<<<<<<<<<<<<<<<<<<
             spacing: 10
-            row_default_height: "500dp"
-            row_force_default: True
-
-            Image:
-                source: "src/assets/zaxxon.zip"
-                size_hint: None, None
-                size: 500, 500
-                allow_stretch: True
-                keep_ratio: False
-                anim_delay: 0
-                anim_loop: -1
+            row_default_height: "300dp"
+            col_default_width: "200dp"
+            col_force_default: True
 
 """
 
@@ -44,8 +36,8 @@ class MainApp(MDApp):
     def on_start(self, *_, **__):
         grid = self.root.ids["images_grid"]
 
-        # for game in gameList.values():
-        #     grid.add_widget(Game(game))
+        for game in gameList.values():
+            grid.add_widget(Game(game))
 
 
 MainApp().run()
